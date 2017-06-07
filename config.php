e2eadf80eafa3677181d946065149cdeba666c62
Xing Qingyu <a1452462323@gmail.com>
2017-06-07 21:17:33 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@ return array(
 	//'配置项'=>'配置值'
 	// 添加数据库配置信息
  'DB_TYPE'   => 'mysql', // 数据库类型
- 'DB_HOST'   => 'me4j7z9j.2310lan.dnstoo.com', // 服务器地址
- 'DB_NAME'   => 'szdata', // 数据库名
- 'DB_USER'   => 'szdata_f', // 用户名
- 'DB_PWD'    => 'xqymain', // 密码
+ 'DB_HOST'   => 'yourhostaddress', // 服务器地址
+ 'DB_NAME'   => 'data_name', // 数据库名
+ 'DB_USER'   => 'user_name', // 用户名
+ 'DB_PWD'    => 'password', // 密码
  'DB_PORT'   => 3306, // 端口
  'DB_PREFIX' => 'icebbs_', // 数据库表前缀
  'DEFAULT_TIMEZONE' => 'PRC', // 默认时区
