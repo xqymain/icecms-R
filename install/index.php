f5cd81bf117fe0e37f35ef572ba3b5aaa180a6e3
Xing Qingyu <a1452462323@gmail.com>
2017-07-13 11:39:19 +0800

    Update index.php

--- install/index.php
+++ install/index.php
@@ -1,6 +1,6 @@
 <?php
 header('content-type:text/html;charset=utf-8');
-echo 'ICECMS程序安装';
+echo 'ICECMS-R Install (PHP7)';
 echo '<form action="install.php"  method="post">';
 echo '数据库地址:<input name="ip" type="txt" value="localhost">(一般不需要修改)<br/>';
 echo '数据库登录名:<input name="user_name" type="txt"><br/>';
